5dcb2ce15b8415044319776769f4e47536be1698
more2024y-dotcom <more2024y@gmail.com>
2026-07-15 02:27:25 +0800

    Create Dashboard.jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,21 @@
+function Dashboard(){
+  return (
+    <div className="dashboard">
+      <h2>OBO User Dashboard</h2>
+
+      <div className="box">
+        Account Status: Active
+      </div>
+
+      <div className="box">
+        Profile Information
+      </div>
+
+      <div className="box">
+        Activity History
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
