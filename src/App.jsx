60e2a68d1279a3262a2d8480a42d40e103467ab8
more2024y-dotcom <more2024y@gmail.com>
2026-07-15 06:11:08 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-
-      <Footer />
-    </>
+    <Home />
   );
 }
 
