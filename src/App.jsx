8183bdabaa7688b6b5739c76c8f17f28dba878e0
more2024y-dotcom <more2024y@gmail.com>
2026-07-15 02:36:37 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,23 +5,3 @@ import Dashboard from "./pages/Dashboard";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
-function App(){
-
-  return (
-    <>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-
-      <Footer />
-    </>
-  );
-}
-
-export default App;
