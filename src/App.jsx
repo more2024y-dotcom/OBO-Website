490778a043b33d6886803c27b141452a89941690
more2024y-dotcom <more2024y@gmail.com>
2026-07-15 04:40:49 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
-
-function App() {
+export default function App() {
   return (
-    <>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-
-      <Footer />
-    </>
+    <div>
+      <h1>OBO Website</h1>
+    </div>
   );
 }
-
-export default App;
