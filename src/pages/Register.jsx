ac7e582bdf31a607980611212a19c5a5d642e288
more2024y-dotcom <more2024y@gmail.com>
2026-07-15 02:25:44 +0800

    Create Register.jsx

---
+++ src/pages/Register.jsx
@@ -0,0 +1,22 @@
+function Register(){
+  return (
+    <div className="card">
+      <h2>Create OBO Account</h2>
+
+      <input placeholder="Full Name" />
+
+      <input placeholder="Email" />
+
+      <input
+        placeholder="Password"
+        type="password"
+      />
+
+      <button>
+        Create Account
+      </button>
+    </div>
+  );
+}
+
+export default Register;
