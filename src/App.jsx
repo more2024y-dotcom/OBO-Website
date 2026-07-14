9eb78375c03baf4c14879a0824dea58c5c44af8c
more2024y-dotcom <more2024y@gmail.com>
2026-07-15 07:21:11 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+
 function App() {
   return (
     <Routes>
-      <Route 
-        path="/" 
-        element={<h1>OBO TEST WORKING</h1>} 
-      />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
